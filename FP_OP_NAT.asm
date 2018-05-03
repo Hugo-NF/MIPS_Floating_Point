f48907f28d147185ad4c606a909917b9f2198e35
Hugo Fonseca <hugonfonseca@hotmail.com>
2018-05-03 18:17:32 -0300

    Comentarios no codigo

--- FP_OP_NAT.asm
+++ FP_OP_NAT.asm
@@ -1,11 +1,14 @@
+# Programa utilizando as instruções nativas do MIPS
 .data
+		# Posições de memória para as variáveis
+A:		.float  0.0		# Primeiro Operando ($f1)
+B:		.float  0.0		# Segundo Operando ($f2)
+C:		.float  0.0		# Resultado ($f12)
 
-B:		.float  0.0		# First Operand	($f1)
-C:		.float  0.0		# Second Operand ($f2)
+OPT:		.word	0		# Codigo de operacao ($s0)
 
-OPT:		.word	0		# Operation Code ($s0)
 
-#Strings for console feedback
+		# Strings para exibição no console
 
 CON_QRY:	.asciiz ">> "
 OPT_MENU:	.asciiz	"\nChoose the operation (Single Precision):\n"
@@ -21,75 +24,77 @@ RES:		.asciiz "\nResult: "
 main:
 
 menu:
-li	$v0, 4			# Print string v0 code
+li	$v0, 4			# Codigo de syscall ($v0) para printar string
 
-la	$a0, OPT_MENU		# Print MENU Text
-syscall
-la	$a0, OPT_ADD		# Print ADD Text
-syscall
-la	$a0, OPT_MULT		# Print SIGN Text
-syscall
-la	$a0, CON_QRY		# Print Console Query
-syscall
+la	$a0, OPT_MENU		# Carrega a string MENU como argumento
+syscall				# Printa MENU
+la	$a0, OPT_ADD		# Carrega a string ADD como argumento
+syscall				# Printa ADD
+la	$a0, OPT_MULT		# Carrega a string MULT como argumento
+syscall				# Printa MULT
+la	$a0, CON_QRY		# Carrega a string CON_QRY como argumento
+syscall				# Printa CON_QRY
 
-li	$v0, 5			# Read integer v0 code
-syscall				# Reading OPT (Operation Code) 
-sw	$v0, OPT
+li	$v0, 5			# Codigo de syscall ($v0) para  code
+syscall				# Lê OPT (Codigo de operacao) 
+sw	$v0, OPT		# Guarda o número lido em OPT
 
 read_operands:
-li	$v0, 4			# Print string v0 code
-la	$a0, OPR_A		# Print "First operand"
-syscall
-la	$a0, CON_QRY		# Print console query
-syscall
+li	$v0, 4			# Codigo de syscall ($v0) para printar string
+la	$a0, OPR_A		# Carrega a string OPR_A como argumento
+syscall				# Printa OPR_A
+la	$a0, CON_QRY		# Carrega a string CON_QRY como argumento
+syscall				# Printa CON_QRY
 
-li	$v0, 6			# Read float v0 code
-syscall
-swc1 	$f0, B			# Save first operand
+li	$v0, 6			# Codigo de syscall ($v0) para leitura de float
+syscall				# Lê o primeiro operando (A)
+swc1 	$f0, A			# Salva o primeiro operando
 
-li	$v0, 4			# Print string v0 code
-la	$a0, OPR_B		# Print "Second operand"
-syscall
-la	$a0, CON_QRY		# Print console query
-syscall
+li	$v0, 4			# Codigo de syscall ($v0) para printar string
+la	$a0, OPR_B		# Carrega a string OPR_B como argumento
+syscall				# Printa OPR_B
+la	$a0, CON_QRY		# Carrega a string CON_QRY como argumento
+syscall				# Printa CON_QRY
 
-li	$v0, 6			# Read float v0 code
-syscall
-swc1 	$f0, C			# Save second operand
+li	$v0, 6			# Codigo de syscall ($v0) para leitura de float
+syscall				# Lê o segundo operando (B)
+swc1 	$f0, B			# Salva o segundo operando
 
-#Choosing between operations (ADD or MULT)
-lw	$s0, OPT
-beq	$s0, $zero, opt_add
-j	opt_mult		
+				# Escolha entre as operacoes (ADD ou MULT)
+lw	$s0, OPT		# Carrega OPT em $s0
+beq	$s0, $zero, opt_add	# Se OPT = 0 pule para opt_add
+		
+opt_mult:			# Subrotina de multiplicação
+lwc1	$f1, A			# Carrega o primeiro operando
+lwc1	$f2, B			# Carrega o segundo operando
+mul.s	$f12, $f1, $f2		# Instrução de multiplicação em ponto flutuante
+swc1	$f12, C			# Salva o resultado em C
+j	display_result		# Salto incondicional para não realizar opt_add
 
-opt_add:
-lwc1	$f1, B
-lwc1	$f2, C
-add.s	$f12, $f1, $f2
-j	display_result
+opt_add:			# Subrotina de soma
+lwc1	$f1, A			# Carrega o primeiro operando
+lwc1	$f2, B			# Carrega o segundo operando
+add.s	$f12, $f1, $f2		# Instrução de soma em ponto flutuante
+swc1	$f12, C			# Salva o resultado em C
 
-opt_mult:
-lwc1	$f1, B
-lwc1	$f2, C
-mul.s	$f12, $f1, $f2
 
 display_result:
-li	$v0, 4			# Print string v0 code
-la	$a0, RES		# Load "result"
-syscall
-li	$v0, 2			# Print float v0 code
-syscall
+li	$v0, 4			# Codigo de syscall ($v0) para printar string
+la	$a0, RES		# Carrega a string RES como argumento 
+syscall				# Printa RES
+li	$v0, 2			# Codigo de syscall ($v0) para printar float
+syscall				# Printa o float presente em $f12 (Resultado)
 
 repeat:
-li	$v0, 4			# Print string v0 code
-la	$a0, OPT_RPT		# Print Repeat option
-syscall
-la	$a0, CON_QRY		# Print console query
-syscall
-li	$v0, 5			# Read integer v0 code
-syscall				# Reading integer 
-beq	$v0, $zero, main	# Repeating if 0 
+li	$v0, 4			# Codigo de syscall ($v0) para printar string
+la	$a0, OPT_RPT		# Carrega a string OPT_RPT como argumento 
+syscall				# Printa OPT_PRT
+la	$a0, CON_QRY		# Carrega a string CON_QRY como argumento
+syscall				# Printa CON_QRY
+li	$v0, 5			# Codigo de syscall ($v0) ler inteiro
+syscall				# Lê inteiro 
+beq	$v0, $zero, main	# Se o inteiro lido for 0, pula para o início do programa 
 
 end_prog:
-li	$v0, 10			# Call to finish program 
-syscall
+li	$v0, 10			# Codigo de syscall ($v0) para encerrar o programa
+syscall				# Encerra
